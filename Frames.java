87cde1da81c54577f3a8d13db1aa038bcdf3876e
Sudhamsu <academyoffice79@gmail.com>
2025-04-13 21:48:35 -0400

    Started Java Gui tutorial

---
+++ Frames.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Frames{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame(); //creates a frame
+        frame.setSize(420,420); //Sets size of frame to 420x420
+        frame.setVisible(true); //make frame visible
+
+    }
+}
