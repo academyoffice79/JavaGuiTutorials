40dd159a246e31bee49e96793275a1578d9c9d9a
Sudhamsu <academyoffice79@gmail.com>
2025-04-16 08:59:00 -0400

    Continue

--- Frames.java
+++ Frames.java
@@ -3,6 +3,7 @@ import javax.swing.JFrame;
 public class Frames{
     public static void main(String[] args) {
         JFrame frame = new JFrame(); //creates a frame
+        frame.setTitle("Frame title goes here");
         frame.setSize(420,420); //Sets size of frame to 420x420
         frame.setVisible(true); //make frame visible
 
